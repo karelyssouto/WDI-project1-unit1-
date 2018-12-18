8284f961a8078b318a60c968d4522a5a0d33b34e
Karelys Souto <karelyssouto@icloud.com>
2018-12-17 19:30:52 -0500

    variables created

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log('testing testing 1 2 3')
+
+// ////////////////
+//    CONNECT 4
+// \\\\\\\\\\\\\\\
+
+// step 1: declare variables for columns, the grid, the rows and the first player
+let grid = [] //an empty array that will take in the values of the moves
+let player = 'player1' //player1 is going to be a present value for the game to start
+let columns = 7
+let rows = 6
+
